e2da14932c37334e566f67c1e9367ff098f987c2
Felipe Felix <115411900+FelipeFelixhub@users.noreply.github.com>
2023-05-01 22:01:12 -0300

    melhorias utilizando ResponseEntity, JpaRepository e organizando o CRUD com a classe Service

--- CriandoAPI/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
+++ CriandoAPI/projeto/src/main/java/br/com/criandoapi/projeto/controller/UsuarioController.java
@@ -1,9 +1,7 @@
 package br.com.criandoapi.projeto.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -15,40 +13,41 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.criandoapi.projeto.DAO.IUsuario;
 import br.com.criandoapi.projeto.model.Usuario;
+import br.com.criandoapi.projeto.service.UsuarioService;
 
 @RestController
 @CrossOrigin("*")
 @RequestMapping("/usuarios")
 public class UsuarioController {
 	
-	@Autowired
-	private IUsuario dao;
+	
+	private UsuarioService usuarioService;
+	
+	public UsuarioController(UsuarioService usuarioService) {
+		this.usuarioService = usuarioService;
+	}
 	
 	// Endpoints - Listar, criar, atualizar e deletar
 	
 	@GetMapping
 	public ResponseEntity<List<Usuario>> listaUsuarios () {
-		List<Usuario> lista = (List<Usuario>) dao.findAll();
-		return ResponseEntity.status(200).body(lista);
+		return ResponseEntity.status(200).body(usuarioService.listarUsuario());
 	}
 	
 	@PostMapping
 	public ResponseEntity<Usuario> criarUsuario (@RequestBody Usuario usuario) {
-		Usuario usuarioNovo = dao.save(usuario);
-		return ResponseEntity.status(201).body(usuarioNovo);
+		return ResponseEntity.status(201).body(usuarioService.criarUsuario(usuario));
 	}
 	
 	@PutMapping
 	public ResponseEntity<Usuario> editarUsuario (@RequestBody Usuario usuario) {
-		Usuario usuarioNovo = dao.save(usuario);
-		return ResponseEntity.status(201).body(usuarioNovo);
+		return ResponseEntity.status(200).body(usuarioService.editarUsuario(usuario));
 	}
 	
 	@DeleteMapping("/{id}")
 	public ResponseEntity<?> excluirUsuario (@PathVariable Integer id) {
-		dao.deleteById(id);
+		usuarioService.excluirUsuario(id);
 		return ResponseEntity.status(204).build();
 	}
 
